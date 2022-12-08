9544f05388213a539d12ad2998354639122dabbe
Gold14567 <70439470+Gold14567@users.noreply.github.com>
2022-12-08 11:28:11 +0100

    Update cablecar_controller.lua

--- cablecar_controller.lua
+++ cablecar_controller.lua
@@ -1,6 +1,6 @@
-RegisterServerEvent("omni:cablecar:host:sync")
-AddEventHandler("omni:cablecar:host:sync", function(index, state)
+RegisterServerEvent("omni:sync")
+AddEventHandler("omni:sync", function(index, state)
     if source == tonumber(GetPlayers()[1]) then
-        TriggerClientEvent("omni:cablecar:forceState", -1, index, state)
+        TriggerClientEvent("omni:forceState", -1, index, state)
     end
 end)
